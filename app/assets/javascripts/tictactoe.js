17d7f7467a7a70bd9fd567ecad693067d515d602
Veronica Bustabad <vbustabad@gmail.com>
2018-10-09 20:43:33 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,13 +1,9 @@
-function player() {
-  if (window.turn % 2 === 0) {
-    return 'X';
-  } else {
-    return 'O';
-  }
-}
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+
+var player = () => turn % 2 ? 'O' : 'X' 
 
 function updateState(clicked_square) {
-  token = player();
+  var token = player();
   clicked_square.innerHTML = token;
 }
 
@@ -16,20 +12,56 @@ function setMessage(string) {
 }
 
 function checkWinner() {
-  if squares[0] === squares[1] === squares[2] || squares[3] === squares[4] === squares[5] || squares[6] === squares[7] === squares[8] {
-    return true;
-  } else if { squares[0] === squares[4] === squares[8] || squares[2] === squares[4] === squares[6]
-    return true;
-  } else { squares[0] === squares[3] === squares[6] || squares[1] === squares[4] === squares[7] || squares[2] === squares[5] === squares[8]
-    return true;
-  }
+  console.log("Check winner.");
+
+  var board = {};
+  var winner = false;
+
+  var input = (index, square) => (
+    board[index] = square
+  );
+
+  $('td').text(input);
+
+  WIN_COMBINATIONS.some(
+    function(array) {
+      if (board[array[0]] !== "" && 
+        board[array[0]] === board[array[1]] &&
+        board[array[1]] === board[array[2]]) {
+      setMessage(`Player ${board[array[0]]} Won!`);
+      return winner = true;
+   }
+  });
+  
+  return winner;
 }
 
-function doTurn() {
-  window.turn += 1;
-  updateState(clicked_square);
-  checkWinner();
+function resetBoard() {
+  turn = 0;
+  $('td').empty;
 }
+
+function doTurn(clicked_square) {
+  updateState(clicked_square);
+  window.turn += 1;
+  
+  if (checkWinner()) {
+    saveGame();
+    resetBoard();
+  } else if (turn === 9) {
+    setMessage("Tie game.");
+    resetBoard();
+  } 
+}
+
+// function saveGame() {
+// }
 
 function attachListeners() {
+  $('td').on('click', function() {
+      doTurn(this);
+  });
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }

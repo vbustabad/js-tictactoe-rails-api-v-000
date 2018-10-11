131c7c4fe5921c50d94bc831ef92d677650763ec
Veronica Bustabad <vbustabad@gmail.com>
2018-10-11 02:53:02 +0000

    Done.

--- app/assets/javascripts/tictactoe.js
+++ app/assets/javascripts/tictactoe.js
@@ -1,10 +1,14 @@
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
+const WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
 
 var turn = 0;
 
 var currentGame = 0;
 
-var player = () => turn % 2 ? 'O' : 'X' 
+$(document).ready(function() {
+    attachListeners();
+});
+
+var player = () => turn % 2 ? 'O' : 'X'
 
 function updateState(clicked_square) {
   var token = player();
@@ -15,10 +19,10 @@ function setMessage(string) {
   document.getElementById('message').innerHTML = string;
 }
 
-function checkWinner() {
-  console.log("Check winner.");
+function checkWinner(){
 
   var board = {};
+  
   var winner = false;
 
   var input = (index, square) => (
@@ -29,18 +33,17 @@ function checkWinner() {
 
   WIN_COMBINATIONS.some(
     function(array) {
-      if (board[array[0]] !== "" && 
+      if (board[array[0]] !== "" &&
         board[array[0]] === board[array[1]] &&
         board[array[1]] === board[array[2]]) {
       setMessage(`Player ${board[array[0]]} Won!`);
       return winner = true;
-   }
-  });
-  
+     }
+   });
   return winner;
 }
 
-function resetBoard() {
+function resetBoard(){
   $('td').empty();
   turn = 0;
   currentGame = 0;
@@ -48,19 +51,19 @@ function resetBoard() {
 
 function doTurn(clicked_square) {
   updateState(clicked_square);
-  turn += 1;
-  
+  turn++;
+
   if (checkWinner()) {
     saveGame();
     resetBoard();
   } else if (turn === 9) {
     setMessage("Tie game.");
+    saveGame();
     resetBoard();
-  } 
+  }
 }
 
-function saveGame() {
-  console.log("In saveGame");
+function saveGame(){
 
   var state = [];
 
@@ -68,37 +71,73 @@ function saveGame() {
      state.push(square);
    });
 
-  game_data = {state: state};
-
+   game_data = {state: state};
+   
   if (currentGame) {
     $.ajax({
-        type: 'PATCH',
-        url: `/games/${currentGame}`,
-        data: game_data
-      });
+      type: 'PATCH',
+      url: `/games/${currentGame}`,
+      data: game_data
+    });
   } else {
-        $.post('/games', game_data, (game) => { currentGame = game.data.id; });
+        $.post('/games', game_data, (game) => {
+          currentGame = game.data.id;
+        });
       }
 }
 
 function showPreviousGames() {
-  console.log("In showPreviousGames");
-  $.get('/games', savedGames)
-    // .done(function(data)){
-    //   data.forEach(createButton)
-    //   var createButton = document.createElement("button");
-    //   button.innerHTML 
-    //   });
-  };
+  $('#games').empty();
+  $.get('/games', (savedGames) => {
+    if (savedGames.data.length) {
+      savedGames.data.forEach(previousGameButtons)
+    }
+  });
+}
+
+ function previousGameButtons(game) {
+   $('#games').append(`<button id="gameid-${game.id}">${game.id}</button><br>`);
+    $(`#gameid-${game.id}`).on('click', () => reloadGame(game.id));
+ }
+
+ function reloadGame(game_id){
+   document.getElementById('message').innerHTML = '';
+
+   const xhr = new XMLHttpRequest;
+   xhr.overrideMimeType('application/json');
+   xhr.open('GET', `/games/${game_id}`, true);
+   xhr.onload = () => {
+     const data = JSON.parse(xhr.responseText).data;
+     const id = data.id;
+     const state = data.attributes.state;
+
+     let index = 0;
+     for (let y = 0; y < 3; y++) {
+       for (let x = 0; x < 3; x++) {
+         document.querySelector(`[data-x="${x}"][data-y="${y}"]`).innerHTML = state[index];
+         index++;
+       }
+     }
+
+     turn = state.join('').length;
+     currentGame = id;
+
+     if (!checkWinner() && turn === 9) {
+       setMessage('Tie game.');
+     }
+   };
+
+   xhr.send(null);
+ }
 
 function attachListeners(){
-   $('td').on('click', function() {
-      if(!$.text(this) && !checkWinner()){
-        doTurn(this);
-      }
-   });
+  $('td').on('click', function() {
+    if(!$.text(this) && !checkWinner()){
+      doTurn(this);
+    }
+ });
 
-   $('#save').on('click', () => saveGame());
-   $('#previous').on('click', () => showPreviousGames());
-   $('#clear').on('click', () => resetBoard());
+  $('#save').on('click', () => saveGame());
+  $('#previous').on('click', () => showPreviousGames());
+  $('#clear').on('click', () => resetBoard());
 }
